b9eb17385fca677a6376769878e2d519b97e9523
desireesdata <joel.feral@sfr.fr>
2025-04-28 11:23:23 +0200

    convertisseur JSON vers CSV pour faciliter diff

---
+++ benchmark/json_to_csv.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import json
+
+def json_to_csv(json_file: str, csv_file: str):
+    try:
+        with open(json_file, 'r') as file:
+            data = json.load(file)
+        entries = data.get("entry", [])
+        df = pd.DataFrame(entries)
+        required_columns = ["nom", "fonction", "adresse"]
+        if not all(column in df.columns for column in required_columns):
+            raise ValueError("Le fichier JSON a un probleme")
+        df.to_csv(csv_file, sep=';', index=False, columns=required_columns)
+
+        print(f"Zou: {csv_file}")
+    except Exception as e:
+        print(f"Erreur : {e}")
+
+json_to_csv('../output/entries_from_ocr.json', 'json_converted.csv')
